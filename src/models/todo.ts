39c73ed77c355210b25efdf51fd559261bbf8be7
Mateus Colombo Godoy <mateuscgodoy@gmail.com>
2024-07-30 09:33:41 -0300

    refactor: moved Todo class into its own file

---
+++ src/models/todo.ts
@@ -0,0 +1,55 @@
+type TodoDTO = {
+  id: number;
+  task: string;
+  completed: boolean;
+};
+
+export class Todo {
+  private _task: string;
+  private _id: number;
+  private _completed: boolean = false;
+
+  constructor(text: string, id?: number, completed?: boolean) {
+    this._task = text;
+    this._id = id ?? Date.now();
+    this._completed = completed ?? false;
+  }
+
+  get id(): number {
+    return this._id;
+  }
+
+  get task(): string {
+    return this._task;
+  }
+
+  get completed(): boolean {
+    return this._completed;
+  }
+
+  set completed(value: boolean) {
+    this._completed = value;
+  }
+
+  public display(): string {
+    return `ID: ${this.id} - ${this._task} - ${
+      this.completed ? "[✅ COMPLETED]" : "❌"
+    }`;
+  }
+
+  public serialize(): TodoDTO {
+    return {
+      id: this._id,
+      task: this._task,
+      completed: this._completed,
+    };
+  }
+
+  public static parse(json: {
+    id: number;
+    task: string;
+    completed: boolean;
+  }): Todo {
+    return new Todo(json.task, json.id, json.completed);
+  }
+}
