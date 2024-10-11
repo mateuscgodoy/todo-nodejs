27a0154ba10fc2643127bc734f07c54f7921ee3e
Mateus Colombo Godoy <mateuscgodoy@gmail.com>
2024-10-11 09:07:32 -0300

    feat: add swagger UI configuration to the application

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
+import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import cors from 'cors';
 
+import swaggerOpts from './swaggerOpts.js';
+
+const PORT = 3000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerOpts));
+
+app.listen(PORT, () => {
+  console.log(`Server located at: http://localhost:${PORT}`);
+});
---
+++ src/swaggerOpts.ts
@@ -0,0 +1,35 @@
+import swaggerJSDoc from 'swagger-jsdoc';
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Todo API',
+      version: '1.0.0',
+    },
+    components: {
+      schemas: {
+        Todo: {
+          type: 'object',
+          properties: {
+            id: {
+              type: 'integer',
+            },
+            title: {
+              type: 'string',
+            },
+            assignedTo: {
+              type: 'string',
+            },
+            done: {
+              type: 'boolean',
+            },
+          },
+        },
+      },
+    },
+  },
+  apis: ['./src/index.ts'],
+};
+
+export default swaggerJSDoc(options);
