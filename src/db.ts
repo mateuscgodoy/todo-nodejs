3e59b32e8b26f0d443f941b03cfc8f5d0033ba46
Mateus Colombo Godoy <mateuscgodoy@gmail.com>
2024-10-11 10:55:23 -0300

    fix: change SQLite import to solve shadowing problem

--- src/db.ts
+++ src/db.ts
@@ -1,28 +1,28 @@
-import { Database } from 'sqlite3';
+import sqlite from 'sqlite3';
 
 export default class TodoDB {
-  private static instance: Database;
+  private _instance: sqlite.Database;
 
-  static getDb(dbPath: string): Database {
-    if (!TodoDB.instance) {
-      TodoDB.instance = new Database(dbPath, (err) => {
-        if (err) {
-          console.error(
-            'Operation aborted: could not connect to SQLite database',
-            err
-          );
-          return;
-        }
-        console.log('Operation complete: Connected to SQLite database');
-      });
+  public get instance(): sqlite.Database {
+    return this._instance;
+  }
 
-      TodoDB.instance.run(`CREATE TABLE IF NOT EXISTS todos (
+  constructor(dbPath: string) {
+    this._instance = new sqlite.Database(dbPath, (err) => {
+      if (err) {
+        console.error(
+          'Operation aborted: could not connect to SQLite database',
+          err
+        );
+        return;
+      }
+      console.log('Operation complete: Connected to SQLite database');
+    });
+    this._instance.run(`CREATE TABLE IF NOT EXISTS todos (
           id INTEGER PRIMARY KEY AUTOINCREMENT,
           title TEXT NOT NULL,
           assignedTo TEXT NOT NULL,
           done INTEGER NOT NULL DEFAULT 0
         )`);
-    }
-    return TodoDB.instance;
   }
 }
