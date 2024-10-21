2fa57fc13cb2f2b5143202d87019f7be8ef211b6
Mateus Colombo Godoy <mateuscgodoy@gmail.com>
2024-10-21 16:24:53 -0300

    feat: add more unit tests for filtering the getTodos function

--- src/tests/queryDatabase.test.ts
+++ src/tests/queryDatabase.test.ts
@@ -75,28 +75,36 @@ describe('QueryDatabase class', () => {
     });
 
     describe('add default todo data to the database', () => {
-      const todos = [
+      const todosData = [
         { title: 'Buy milk', assignedTo: 'Bobby' },
         { title: 'Buy some eggs', assignedTo: 'Bobby' },
         { title: 'Call mom', assignedTo: 'Me' },
+        { title: 'Buy flour', assignedTo: 'Me' },
       ];
       beforeEach(() => {
-        db.insertTodos(todos);
+        db.insertTodos(todosData);
       });
 
       it('get all Todos from the Database', () => {
-        const getTodos = db.getTodos();
-        assert.equal(getTodos.length, 3);
+        const todos = db.getTodos();
+        assert.equal(todos.length, 4);
       });
 
       it('correctly filters Todos by title', () => {
-        const getTodos = db.getTodos({ title: 'Buy' });
-        assert.equal(getTodos.length, 2);
+        const todos = db.getTodos({ title: 'Buy' });
+        assert.equal(todos.length, 3);
       });
 
       it('correctly filters Todos by assignedTo', () => {
         const bobbyTodos = db.getTodos({ assignedTo: 'Bobby' });
         assert.equal(bobbyTodos.length, 2);
+      });
+
+      it('correctly filters Todos by title and assignedTo', () => {
+        const todos = db.getTodos({ title: 'Buy', assignedTo: 'Me' });
+        assert.equal(todos.length, 1);
+        assert.equal(todos[0].assignedTo, todosData[3].assignedTo);
+        assert.equal(todos[0].title, todosData[3].title);
       });
 
       it('correctly gets an Todo by providing a valid ID', () => {
@@ -107,6 +115,35 @@ describe('QueryDatabase class', () => {
       it('correctly returns undefined for unknown IDs', () => {
         const todo = db.getTodos({ id: -1 })[0];
         assert.equal(todo, undefined);
+      });
+
+      it('correctly filters Todos respecting the limit value', () => {
+        const todos = db.getTodos({ limit: 2 });
+        assert.equal(todos.length, 2);
+      });
+
+      it('correctly returns todos based on the provider offset', () => {
+        const todos = db.getTodos({ limit: 2, offset: 1 });
+        assert.equal(todos.length, 2);
+        assert.equal(todos[0].title, todosData[1].title);
+        assert.equal(todos[1].title, todosData[2].title);
+      });
+
+      it('correctly returns todos filtered by title, limit and offset', () => {
+        const todos = db.getTodos({ title: 'Buy', limit: 2, offset: 1 });
+        assert.equal(todos.length, 2);
+        assert.equal(todos[0].title, todosData[1].title);
+        assert.equal(todos[1].assignedTo, todosData[3].assignedTo);
+      });
+
+      it('throws a DatabaseError if offset is used without a limit', () => {
+        assert.throws(
+          () => {
+            const todos = db.getTodos({ offset: 10 });
+          },
+          DatabaseError,
+          'Error: Offset can not be set without a valid limit'
+        );
       });
     });
   });
