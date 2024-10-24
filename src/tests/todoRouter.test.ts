a74f2faff236beb6b35a7b3686b5e8e5e0c59659
Mateus Colombo Godoy <mateuscgodoy@gmail.com>
2024-10-24 09:35:39 -0300

    feat: add integration tests for update and fixes for new validation logic

--- src/tests/todoRouter.test.ts
+++ src/tests/todoRouter.test.ts
@@ -15,6 +15,8 @@ describe('TodoRouter', () => {
   });
 
   describe('GET /todos', () => {
+    // TODO: Add tests for filtering options
+
     it('returns all Todos stored on the application with 200 status', async () => {
       const response = await request.get('/todos');
 
@@ -25,7 +27,7 @@ describe('TodoRouter', () => {
 
   describe('GET /todos/:id', () => {
     it('returns the specific todo if the ID is valid', async () => {
-      const response = await request.get('/todos/' + 1);
+      const response = await request.get('/todos/1');
 
       assert.equal(response.status, 200);
       assert.equal(response.body.title, testTodos[0].title);
@@ -45,50 +47,90 @@ describe('TodoRouter', () => {
       const response = await request.get('/todos/bacon');
 
       assert.equal(response.status, 400);
-      assert.equal(response.body.message, 'The ID provided is invalid');
+      assert.equal(response.body.message, 'The ID parameter must be a number');
     });
   });
 
   describe('POST /todos', () => {
     it('successfully create a new todo with 201 status', async () => {
-      const response = await request
-        .post('/todos')
-        .send({ title: 'A valid Todo', assignedTo: 'John' });
+      const todo = { title: 'A valid Todo ⭐!', assignedTo: 'John' };
+      const response = await request.post('/todos').send({ todo });
 
       assert.equal(response.status, 201);
       assert.equal(response.body.message, 'Todo created with success');
     });
 
     it('fails the creation without a title with status 400 and an error message', async () => {
-      const response = await request
-        .post('/todos')
-        .send({ assignedTo: 'Bobby' });
+      const todo = { assignedTo: 'Bobby' };
+      const response = await request.post('/todos').send({ todo });
 
       assert.equal(response.status, 400);
-      assert.equal(
-        response.body.message,
-        'Please provide a title and an assigned person'
-      );
+      assert.equal(response.body.message, 'Todo title text is required');
     });
 
     it('fails the creation without a assignedTo with status 400 and error message', async () => {
-      const response = await request
-        .post('/todos')
-        .send({ title: 'An invalid todo' });
+      const todo = { title: 'An invalid todo' };
+      const response = await request.post('/todos').send({ todo });
 
       assert.equal(response.status, 400);
       assert.equal(
         response.body.message,
-        'Please provide a title and an assigned person'
+        'An assigned persons name is required'
+      );
+    });
+
+    it('fails the creation if not Todo exists on the request body', async () => {
+      const response = await request.post('/todos').send();
+
+      assert.equal(response.status, 400);
+      assert.equal(response.body.message, 'The Todo sent is invalid');
+    });
+
+    it('fails when the title contains potentially harmful characters', async () => {
+      const todo = {
+        title: 'This todo has <script>dangerous code</script>',
+        assignedTo: 'Bob',
+      };
+
+      const response = await request.post('/todos').send({ todo });
+
+      assert.equal(response.status, 400);
+      assert.equal(
+        response.body.message,
+        'Todo title contain invalid characters'
       );
     });
   });
 
-  describe('UPDATE /todos', () => {
-    // TODO: Implement update integration tests
+  describe('UPDATE /todos/:id', () => {
+    it('successfully update a valid todo', async () => {
+      const todo = { ...testTodos[0], done: true };
+      const response = await request.patch('/todos/1').send({ todo });
+
+      assert.equal(response.status, 200);
+      assert.equal(response.body.message, 'Todo updated with success');
+      assert.equal(response.body.todo.done, true);
+    });
+
+    it('succeeds even when there is no information passed', async () => {
+      const response = await request.patch('/todos/1').send({ todo: {} });
+
+      assert.equal(response.status, 200);
+      assert.equal(response.body.message, 'Todo updated with success');
+    });
+
+    it('fails if the provided ID does not exist with a 404 status and an error message', async () => {
+      const response = await request.patch('/todos/-1').send({ todo: {} });
+
+      assert.equal(response.status, 404);
+      assert.equal(
+        response.body.message,
+        'No Todo was found for the provided id.'
+      );
+    });
   });
 
-  describe('DELETE /todos/:id', () => {
-    // TODO: Implement delete integration tests
-  });
+  // describe('DELETE /todos/:id', () => {
+  //   // TODO: Implement delete integration tests
+  // });
 });
